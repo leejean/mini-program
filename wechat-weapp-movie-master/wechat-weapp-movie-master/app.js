1ecb660a888a2a075971bb9b5f90b44e19d3b98e
leejean <ilove_cp3@163.com>
2017-04-07 00:14:43 +0800

    定位

--- wechat-weapp-movie-master/wechat-weapp-movie-master/app.js
+++ wechat-weapp-movie-master/wechat-weapp-movie-master/app.js
@@ -22,12 +22,12 @@ App({
       }
     })
   },
-  getCity: function(cb) {
+  getCity2: function(cb) {
     var city = '深圳'
     config.city = city
     typeof cb == "function" && cb(city)
   },
-  getCity2: function(cb) {
+  getCity: function(cb) {
     //备份源码
     var that = this
     wx.getLocation({
