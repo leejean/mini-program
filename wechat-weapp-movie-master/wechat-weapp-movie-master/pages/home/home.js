b2fbe2b02f4b6603b7ea64388cea0cbf6cfc4f8c
leejean <ilove_cp3@163.com>
2017-06-06 23:23:48 +0800

    提交

--- wechat-weapp-movie-master/wechat-weapp-movie-master/pages/home/home.js
+++ wechat-weapp-movie-master/wechat-weapp-movie-master/pages/home/home.js
@@ -1,17 +1,15 @@
-var douban = require('../../comm/script/fetch')
 var config = require('../../comm/script/config')
 var info = require('../../comm/script/info')
 var app = getApp()
 Page({
 	data: {
 		server:config.server,
-		films: [],
 		hasMore: true,//是否有更多数据标记
 		showLoading: true,
-		page:1,
-		pagesize:4,
 		bannerList: config.bannerList,
 		queryData:{
+			page:1,
+			pagesize:4,
 			//longitude:113.877012,
 			//latitude:22.575624,
 			//scope:100,
@@ -27,29 +25,25 @@ Page({
 		app.getCity(function(){
 			//隐藏导航条加载动画
 			wx.hideNavigationBarLoading()
-//			wx.setNavigationBarTitle({
-//				title: '发现- ' + config.city
-//			})
+			wx.setNavigationBarTitle({
+				title: '发现'// + config.city
+			})
+			info.getInfos.call(that,that.data.queryData)
 		})
-		that.data.queryData.page=that.data.page;
-		that.data.queryData.pagesize=that.data.pagesize;
-		info.getInfos.call(that, that.data.queryData,function(dto){
-			that.setData({
-	    		infos: that.data.infos.concat(dto.data.rows),
-	   			page:that.data.page+1,
-	    		showLoading: false,
-	    		hasMore:dto.data.page==dto.data.pageCount
-	  		})
-		})
+		
 	},
 	//监听该页面用户下拉刷新事件
 	onPullDownRefresh: function() {
 		var that = this
+		var data = that.data
+		data.page = 1
+		data.keyWord = ''
 		that.setData({
 			infos: [],
 			hasMore: true,
 			showLoading: true,
-			page: 1
+			queryData: data
+			
 		})
 		this.onLoad()
 	},
@@ -57,23 +51,12 @@ Page({
 	onReachBottom: function() {
 		var that = this
 		if (!that.data.showLoading) {
-			that.data.queryData.page=that.data.page;
-			that.data.queryData.pagesize=that.data.pagesize;
-			info.getInfos.call(that, that.data.queryData,function(dto){
-			that.setData({
-	    		infos: that.data.infos.concat(dto.data.rows),
-	   			page:dto.data.rows.length>0?that.data.page+1:dto.data.page,
-	    		showLoading: false,
-	    		hasMore:dto.data.page==dto.data.pageCount
-	    		
-	  		})
-		})
+			info.getInfos.call(that,that.data.queryData)
 		}
 	},
 	//详情
 	viewInfoDetail: function(e) {
 		var data = e.currentTarget.dataset;
-		console.info(data)
 	},
 	//点击轮播图
 	viewBannerDetail: function(e) {
