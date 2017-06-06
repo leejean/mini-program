8cbe0a224aae0026ec37a8c152236566b006f0e1
leejean <ilove_cp3@163.com>
2017-06-06 23:23:07 +0800

    提交

--- wechat-weapp-movie-master/wechat-weapp-movie-master/comm/script/info.js
+++ wechat-weapp-movie-master/wechat-weapp-movie-master/comm/script/info.js
@@ -6,27 +6,47 @@ var httpclient = require('../../util/httpclient')
  * @param {Object} data 请求参数
  * @param {Function} cb 回调函数
  */
-function getInfos(data, cb, cb_fail) {
-  var that = this;
-  
-  httpclient.request(
-  	that.data.server + "api/info/getInfos", 
-  	data, 
-  	'POST',  
-  	function(res){
-      typeof cb == 'function' && cb(res.data)
-    },
-    function() {
-      message.show.call(that,{
-        content: '网络开小差了',
-        icon: 'offline',
-        duration: 3000
-      })
-      typeof cb_fail == 'function' && cb_fail(res.data)
-    }
-  );
+function getInfos(data, cb, fail_cb) {
+	  var that = this
+	  message.hide.call(that)
+	  if (that.data.hasMore) {
+		  httpclient.request(
+		  	that.data.server + "api/info/getInfos", 
+		  	data, 
+		  	'POST',  
+		  	function(res){
+		      	var dto = res.data;
+				 if(dto.data.rows.length === 0){
+		          that.setData({
+		            hasMore: false,
+		          })
+		        }else{
+		          data.page = dto.data.page+1;
+		          that.setData({
+		    		infos: that.data.infos.concat(dto.data.rows),
+		   			queryData:data,
+		    		showLoading: false,
+		    		hasMore:dto.data.page<dto.data.pageCount
+		  		})
+		        }
+		        wx.stopPullDownRefresh()
+		        typeof cb == 'function' && cb(res.data)
+		    },
+		    function() {
+		        that.setData({
+		            showLoading: false
+		        })
+		        message.show.call(that,{
+		          content: '网络开小差了',
+		          icon: 'offline',
+		          duration: 3000
+		        })
+		        wx.stopPullDownRefresh()
+		        typeof fail_cb == 'function' && fail_cb()
+		      }
+		  );
+		}
 }
-
 function save(data, cb, cb_fail) {
   var that = this;
   httpclient.request(
